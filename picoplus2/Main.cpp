2a4fe7d3ef68046b4339760793f72a17b448b092
Charlie Birks <charlie@daft.games>
2024-08-10 18:48:07 +0100

    Initial DVHSTX output
    There goes all my RAM

--- picoplus2/Main.cpp
+++ picoplus2/Main.cpp
@@ -9,6 +9,8 @@
 
 #include "tusb.h"
 
+#include "dvhstx/dvhstx.hpp"
+
 #include "BIOS.h"
 
 #include "AboveBoard.h"
@@ -32,7 +34,9 @@ static FixedDiskAdapter fixDisk(sys);
 
 static SerialMouse mouse(sys);
 
-static uint8_t ram[256 * 1024];
+static uint8_t ram[128 * 1024];
+
+static pimoroni::DVHSTX dv;
 
 static void scanlineCallback(const uint8_t *data, int line, int w)
 {
@@ -40,13 +44,26 @@ static void scanlineCallback(const uint8_t *data, int line, int w)
     if(w > 640)
         w = 640;
 
+    auto fb = dv.get_back_buffer();
+    auto ptr = fb + line * 640;
+
     // convert
+    for(int x = 0; x < w; x += 2)
+    {
+        int index = *data & 0xF;
+        *ptr++ = index;
+        if(w == 320) *ptr++ = index;
+
+        index = *data++ >> 4;
+        *ptr++ = index;
+        if(w == 320) *ptr++ = index;
+    }
 
     if(line == 0)
     {
         // sync
+        dv.wait_for_flip();
         // set_display_size(w, 240); /*really 200*/
-
     }
     
     // write
@@ -54,6 +71,7 @@ static void scanlineCallback(const uint8_t *data, int line, int w)
     if(line == 199)
     {
         // end
+        dv.flip_async();
     }
 }
 
@@ -80,6 +98,36 @@ void update_mouse_state(int8_t x, int8_t y, bool left, bool right)
     mouse.sync();
 }
 
+static void display_init()
+{
+    dv.init(640, 240, pimoroni::DVHSTX::MODE_PALETTE);
+
+    static const uint8_t palette[16][4]
+    {
+        // B, G, R, X
+        {0x00, 0x00, 0x00}, // black
+        {0xAA, 0x00, 0x00}, // blue
+        {0x00, 0xAA, 0x00}, // green
+        {0xAA, 0xAA, 0x00}, // cyan
+        {0x00, 0x00, 0xAA}, // red
+        {0xAA, 0x00, 0xAA}, // magenta
+        {0x00, 0x55, 0xAA}, // brown
+        {0xAA, 0xAA, 0xAA}, // light grey
+
+        {0x55, 0x55, 0x55}, // dark grey
+        {0xFF, 0x55, 0x55}, // light blue
+        {0x55, 0xFF, 0x55}, // light green
+        {0xFF, 0xFF, 0x55}, // light cyan
+        {0x55, 0x55, 0xFF}, // light red
+        {0xFF, 0x55, 0xFF}, // light magenta
+        {0x55, 0xFF, 0xFF}, // yellow
+        {0xFF, 0xFF, 0xFF}, // white
+    };
+
+    for(int i = 0; i < 16; i++)
+        dv.set_palette_colour(i, reinterpret_cast<const uint32_t *>(palette)[i]);
+}
+
 int main()
 {
     tusb_init();
@@ -87,7 +135,8 @@ int main()
     stdio_init_all();
 
     // init storage/filesystem
-    // init display
+
+    display_init();
 
     // emulator init
     sys.addMemory(0, sizeof(ram), ram);
