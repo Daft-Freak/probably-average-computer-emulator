e9dd5c13baf0b5b172312e62995756697928b63b
Charlie Birks <charlie@daft.games>
2025-05-13 22:26:42 +0100

    Move PSRAM pin config over to config.h

--- pico2/Main.cpp
+++ pico2/Main.cpp
@@ -11,6 +11,8 @@
 #include "tusb.h"
 
 #include "fatfs/ff.h"
+
+#include "config.h"
 #include "psram.h"
 
 #include "BIOS.h"
@@ -24,14 +26,6 @@
 #include "Scancode.h"
 #include "SerialMouse.h"
 #include "System.h"
-
-#ifdef PIMORONI_PICO_PLUS2_RP2350
-#define PSRAM_CS_PIN PIMORONI_PICO_PLUS2_PSRAM_CS_PIN
-#elif defined(SOLDERPARTY_RP2350_STAMP_XL)
-#define PSRAM_CS_PIN 8
-#else
-#error "No PSRAM CS!"
-#endif
 
 static FATFS fs;
 
--- pico2/config.h
+++ pico2/config.h
@@ -9,9 +9,16 @@
 #define SD_MOSI   37
 #define SD_MISO   38
 #define SD_CS     36
-#else
+
+#define PSRAM_CS_PIN 8
+
+#elif defined(PIMORONI_PICO_PLUS2_RP2350)
 #define SD_SCK  2
 #define SD_MOSI 4
 #define SD_MISO 3
 #define SD_CS   5
+
+#define PSRAM_CS_PIN PIMORONI_PICO_PLUS2_PSRAM_CS_PIN
+#else
+#error "No board configuration!"
 #endif
