ee44a06f5a9b7d1396c21dbe17e559658549ca3c
Charlie Birks <charlie@daft.games>
2025-07-21 22:57:01 +0100

    Split the PSRAM CS config out of the main block

--- pico2/config.h
+++ pico2/config.h
@@ -1,5 +1,13 @@
 #pragma once
 
+// PSRAM config first as that only depends on the board
+// and not any additional thing it's plugged into
+#ifdef SOLDERPARTY_RP2350_STAMP_XL
+#define PSRAM_CS_PIN 8
+#elif defined(PIMORONI_PICO_PLUS2_RP2350)
+#define PSRAM_CS_PIN PIMORONI_PICO_PLUS2_PSRAM_CS_PIN
+#endif
+
 #ifdef SOLDERPARTY_RP2350_STAMP_XL
 #define DVI_CLK_P 14
 #define DVI_D0_P  12
@@ -11,8 +19,6 @@
 #define SD_MOSI   37
 #define SD_MISO   38
 #define SD_CS     36
-
-#define PSRAM_CS_PIN 8
 
 #elif defined(PIMORONI_PICO_PLUS2_RP2350)
 // as I was using a mess of jumper wires, there's not really a right answer here
@@ -26,7 +32,6 @@
 #define SD_MISO 3
 #define SD_CS   5
 
-#define PSRAM_CS_PIN PIMORONI_PICO_PLUS2_PSRAM_CS_PIN
 #else
 #error "No board configuration!"
 #endif
