e3cdc412e0268095a01425fcaa7e0bd4732568c8
Charlie Birks <charlie@daft.games>
2025-07-23 23:43:45 +0100

    Fix reading CGA vsync status
    We were always returing 1, whoops

--- core/CGACard.cpp
+++ core/CGACard.cpp
@@ -126,7 +126,7 @@ uint8_t CGACard::read(uint16_t addr)
             // the low "accessible" bit is the inverse of DE
             bool de = scanlineCycle < hDisplayed && scanline < vDisplayed;
 
-            return 0xF0 | status | (de ? 0 : 1);
+            return 0xE0 | status | (de ? 0 : 1);
         }
     }
     return 0xFF;
